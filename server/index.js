aa81a411f65a4201a783581343bc792ed8ffb2d9
Shohei Kikuchi <killchance2256@gmail.com>
2022-04-16 00:38:37 +0900

    feat: Add get_list

--- server/index.js
+++ server/index.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const app = express()
+const { uuid } = require  ('uuidv4');
 const port = 3001
 
 app.get('/', (req, res) =>{
   res.send('Hello World')
+})
+
+const TodoStatus = {
+  inProgress: 0,
+  done: 1,
+}
+
+let todoList = [
+  {
+    id: uuid(),
+    todo: 'hogehoge',
+    status: TodoStatus.inProgress
+  }
+]
+
+app.get("/get_list", (req, res) => {
+  res.json({data: todoList})
 })
 
 app.listen(port, () => {
