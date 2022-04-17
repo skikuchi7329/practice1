3731e7fdd263800c1e46cc36ff4f876bfa0029ec
Shohei Kikuchi <killchance2256@gmail.com>
2022-04-17 23:19:45 +0900

    feat: fetch list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { useEffect, useState } from "react";
 
+const App = () => {
+  const [list, setList] = useState([]);
 
-function App() {
+  useEffect(() => {
+    fetch("http://localhost:3001/get_list")
+      .then((res) => response.json())
+      .then((result) => {
+        setList(result.data);
+      });
+  }, []);
+
   return (
     <div className="App">
-      hello world
+      <ul>
+      {list && list.map((i) => <li key={i.id}>{i.todo} | {i.id} | {i.status ? '✅': '☑️'}</li>)}
+      </ul>
     </div>
-  );
-}
+  )
+};
 
 export default App;
