4bd614315d4eb2c497abeb6056fc214155bba4b6
Shohei Kikuchi <killchance2256@gmail.com>
2022-04-23 00:03:15 +0900

    feat: Add create button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import { useEffect, useState, useCallback } from "react";
 import { Todo } from "./components/Todo";
-const axios = require('axios');
+const axios = require("axios");
 
 const App = () => {
   const [list, setList] = useState([]);
 
+  const [keyword, setKeyword] = useState("Type Something");
+
   const fetchData = useCallback(async () => {
-    const res = await axios.get("http://localhost:3001/get_list")
+    const res = await axios.get("http://localhost:3001/get_list");
     setList(res.data.data);
-  }, [])
+  }, []);
 
   useEffect(() => {
-    fetchData()
+    fetchData();
   }, [fetchData]);
+
+  const handleChange = (e) => {
+    setKeyword(e.target.value);
+  };
+
+  const handlCreate = async () => {
+    await axios.post("http://localhost:3001/create", { todo: keyword });
+    fetchData();
+  };
 
   return (
     <div className="App">
+      <input value={keyword} onChange={handleChange} />
+      <button onClick={handlCreate}>Create</button>
       <ul>
-        {list && list.map(({id, todo, status}) => <Todo key={id} id={id} todo={todo} status={status} />)}
+        {list &&
+          list.map(({ id, todo, status }) => (
+            <Todo key={id} id={id} todo={todo} status={status} />
+          ))}
       </ul>
     </div>
   );
